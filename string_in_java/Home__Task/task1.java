73439d4a973eae81507a3ff1e3ac6647fe23393c
Gul-Is-Here <98545209+Gul-Is-Here@users.noreply.github.com>
2023-09-16 21:12:25 +0500

    "Home Task of Strings"

---
+++ string_in_java/Home__Task/task1.java
@@ -0,0 +1,9 @@
+package string_in_java.Home__Task;
+
+public class task1 {
+    /*
+     * Take an array of String input from user and find the cumulative (combined)
+     * length of all those Strings
+     */
+
+}
---
+++ string_in_java/toString_method.java
@@ -0,0 +1,11 @@
+package string_in_java;
+
+public class toString_method {
+    public static void main(String[] args) {
+        // Using to String method of string class
+        // Always Remember java Stirng are immutable
+        int numbers = 123;
+        String str = Integer.toString(numbers);
+        System.out.println(str.length());
+    }
+}
